a415d30ccfe82bae983bfc4354df1836ff605c70
riosmatias1981 <134092222+riosmatias1981@users.noreply.github.com>
2024-04-14 17:23:55 -0300

    Add files via upload

--- funcion.js
+++ funcion.js
@@ -5,16 +5,21 @@ let listadoDeProductos = 'Listado de Productos\n\n';
 function solicitarDatosUsuario() {
     let nombre = prompt('Ingrese su nombre');
     let apellido = prompt('Ingrese su Apellido');
-    alert('Bienvenido '+ nombre.toUpperCase() + ' ' + apellido.toUpperCase());
+    alert('Bienvenido '+ nombre.toUpperCase() + ' ' + apellido.toUpperCase()
+);
 }
 
-function agregarProductos() {
-    let confirmacion = confirm('Desea agregar un producto a su carrito?');
+function elegirDesayuno() {
+    let confirmacion = confirm('Ingrese la letra correspondiente al desayuno de su elección'
+ 
+    +'\n\n Ingrese A - Desayuno Express $18000'
+    +'\n Ingrese B - Desayuno Ejecutivo $25.000'
+    +'\n Ingrese C - Desayuno Para 2 $45.000');
     
     while (confirmacion){
-        introducirDatosProducto();
+        ingresarSeleccionDesayuno();
 
-        confirmacion = confirm('Desea agregar un producto a su carrito?');
+        confirmacion = confirm('Desea elegir otro desayuno?');
 
     }
 
@@ -23,32 +28,58 @@ function agregarProductos() {
     }
 }
 
-function introducirDatosProducto() {
-    let nombre, precio, cantidad, total;
+function ingresarSeleccionDesayuno() {
+    let nombreDesayuno, tipoDesayuno, ramoFlores, total;
 
-    nombre = prompt('Ingrese el Nombre del Producto');
-    precio = parseFloat(prompt('Ingrese el precio del Producto'));
-    cantidad = parseInt(prompt('Ingrese la cantidad de Productos'));
-    total = calcularTotal(precio, cantidad);
+    tipoDesayuno = prompt('Ingrese la letra que corresponda a su desayuno A, B o C');
+    ramoFlores = prompt('Quisiera agregar un ramo de flores por $5.000?\n ¿SI o NO?');
+    
+    if(tipoDesayuno == 'A'){
+        tipoDesayuno = '18000'
+        nombreDesayuno = 'EXPRESS'
+    }
+    else if(tipoDesayuno == 'B'){
+        tipoDesayuno = '25000'
+        nombreDesayuno = 'EJECUTIVO'
+    }
+    else if(tipoDesayuno == 'C'){
+        tipoDesayuno = '45000'
+        nombreDesayuno = 'PARA 2'
+    }
+
+    if(ramoFlores == 'SI') {
+        ramoFlores = '5000'
+    }
+    else if(ramoFlores == 'NO') {
+        ramoFlores = '0'
+    }
+
+    total = calcularTotal(tipoDesayuno, ramoFlores);
     contadorDeProductos++;
 
-    console.log({nombre, precio, cantidad, total});
-    listadoDeProductos +='Producto' + contadorDeProductos + '\n|Nombre: '+ nombre + '|Precio: ' + precio + '|Cantidad' + cantidad + '\n|Total $' + total + '\n'
+    console.log({tipoDesayuno, nombreDesayuno, ramoFlores: ramoFlores, total});
+    listadoDeProductos +='\n |Desayuno ' + contadorDeProductos + '\n Tipo de Desayuno: '+ nombreDesayuno + ' $' + tipoDesayuno + '\n Ramo de Flores: ' + ramoFlores +  '\n|Total $' + total + '\n'
+
+
+    function calcularTotal(tipoDesayuno){
+        let total = parseInt(tipoDesayuno) + parseInt(ramoFlores);
+        totalGeneral += total;
+    
+        return total;
+    
+    }
+
 }
 
-function calcularTotal(precio, cantidad){
-    let total = precio * cantidad;
-    totalGeneral += total;
 
-    return total;
 
-}
+
 
 function mostrarInfo(){
-    listadoDeProductos +='\n\nEl total a pagar de todos los productos es  de $' + totalGeneral;
+    listadoDeProductos +='\n\n||El total a pagar de todos los productos es  de $' + totalGeneral + '||';
     alert(listadoDeProductos);
     console.log(listadoDeProductos);
 }
 
 solicitarDatosUsuario();
-agregarProductos();
+elegirDesayuno();
